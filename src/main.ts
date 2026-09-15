288cabb661fd30133e7c29d45785401fd25fd9e8
JUANS <iberiantestgc@gmail.com>
2026-09-15 22:27:45 +0200

    Configura persistencia de autorizacion en Swagger

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,12 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
 
   const port = configService.get<number>('PORT', 3000);
 
